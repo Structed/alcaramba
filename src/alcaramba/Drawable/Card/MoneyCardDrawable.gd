9c2057c455d1fc22ef1ea92acfa5721e2dd0b95c
Johannes Ebner <Structed@users.noreply.github.com>
2022-01-07 22:18:20 +0100

    IMplemented displaying of proper color on card

--- src/alcaramba/Drawable/Card/MoneyCardDrawable.gd
+++ src/alcaramba/Drawable/Card/MoneyCardDrawable.gd
@@ -7,8 +7,8 @@ var _card_info: MoneyCard
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Value.text = str(_card_info._value)
-	$Value.modulate = Color(0,0,0)
-	modulate = Color(255, 106, 0)
+	$Value.self_modulate = Color(0,0,0)
+	self_modulate = _card_info.get_color()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- src/alcaramba/Scripts/MoneyCard.gd
+++ src/alcaramba/Scripts/MoneyCard.gd
@@ -9,3 +9,19 @@ enum Currency {BLUE, YELLOW, GREEN, ORANGE}
 
 # Of type enum Currency
 var _currency: int
+
+func get_color() -> Color:
+	var color: Color
+	match _currency:
+		Currency.BLUE:
+			color = Color(0.14902, 0.360784, 1)
+		Currency.YELLOW:
+			color = Color(1, 0.890196, 0.278431)
+		Currency.GREEN:
+			color = Color(0.14902, 0.498039, 0)
+		Currency.ORANGE:
+			color = Color(1, 0.415686, 0)
+		_:
+			color = Color(1, 1, 1)
+	
+	return color
