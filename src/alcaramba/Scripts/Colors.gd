037490f7c525717ff5142f4b8f0c3fd07524f87b
Johannes Ebner <Structed@users.noreply.github.com>
2022-01-08 02:03:36 +0100

    Set default color to transparent

--- src/alcaramba/Scripts/Colors.gd
+++ src/alcaramba/Scripts/Colors.gd
@@ -1,7 +1,7 @@
 extends Object
 class_name Colors
 
-const DEFAULT = Color(1, 1, 1)
+const DEFAULT = Color(1, 1, 1, 0)
 const BLUE = Color(0.14902, 0.360784, 1)
 const YELLOW  = Color(1, 0.890196, 0.278431)
 const GREEN = Color(0.14902, 0.498039, 0)
