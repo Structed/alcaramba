a38b02afe087a87c21830cb848ed517bbdb13c2e
zientist <jakob.ebner@gmail.com>
2022-01-25 22:58:57 +0100

    added states for removing and replacing tiles
    Also added add and remove card functions to Abstract card collection

--- src/alcaramba/Scripts/AbstractCardCollection.gd
+++ src/alcaramba/Scripts/AbstractCardCollection.gd
@@ -20,3 +20,14 @@ func get_card_by_id(id: int):
 		if card._id == id:
 			return card
 	return null
+	
+func add_card(card):
+	_stack.append(card)
+
+# removes all cards with certain id
+func remove_card_by_id(id: int):
+	for card in _stack:
+		if card._id == id:
+			_stack.erase(card)
+
+
