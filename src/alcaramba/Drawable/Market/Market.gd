0ce8b113e3241891d47f788dda895e787fac2842
Johannes Ebner <Structed@users.noreply.github.com>
2022-12-26 20:49:06 +0100

    Refactor: Split Money Market out of Market

--- src/alcaramba/Drawable/Market/Market.gd
+++ src/alcaramba/Drawable/Market/Market.gd
@@ -2,17 +2,17 @@ extends Control
 
 const MAX_MARKET_CARDS = 4
 
-var card_scene = preload("res://Drawable/Card/MoneyCardDrawable.tscn")
 var tile_card_scene = preload("res://Drawable/Card/TileCardDrawable.tscn")
 
 onready var end_turn_button = get_node("%EndTurnButton")
 onready var tile_market = get_node("%TileMarket")
+onready var money_market = get_node("%MoneyMarket")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.stack_tiles.shuffle()
-
+	money_market.connect("money_card_selected", self, "_on_money_card_selected")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -23,12 +23,8 @@ func _process(_delta):
 		end_turn_button.disabled = true
 
 
-func _get_active_money_card_count() -> int:
-	return $MarketsPanel/MoneyMarket/MoneyCards.get_child_count()
-
-
 func _check_can_refill_cards() -> bool:
-	if _get_active_money_card_count() < MAX_MARKET_CARDS || tile_market.cards_can_be_refilled():
+	if money_market.cards_can_be_refilled() || tile_market.cards_can_be_refilled():
 		return true
 	else:
 		return false
@@ -36,18 +32,8 @@ func _check_can_refill_cards() -> bool:
 
 func _on_EndTurnButton_pressed():
 	tile_market.refill_tiles()
-
-	var cards_to_refill_count = MAX_MARKET_CARDS - _get_active_money_card_count()
-	for _i in range(cards_to_refill_count):
-		var card = Global.stack_money.take_card()
-		var card_node = card_scene.instance()
-		card_node._card_info = card
-		$MarketsPanel/MoneyMarket/MoneyCards.add_child(card_node)
-		card_node.connect("pressed", self, "_on_MoneyCard_pressed", [card_node])
+	money_market.refill()
 
 
-func _on_MoneyCard_pressed(card_node: MoneyCardDrawable):
-	card_node.disconnect("pressed", self, "_on_MoneyCard_pressed")
-	Global.active_player.money_cards.add_card(card_node._card_info)
-	$MarketsPanel/MoneyMarket/MoneyCards.remove_child(card_node)
+func _on_money_card_selected(card_node: MoneyCardDrawable):
 	$MarketsPanel/Hand/Cards.add_child(card_node)
---
+++ src/alcaramba/Drawable/Market/MoneyMarket.gd
@@ -0,0 +1,32 @@
+extends HBoxContainer
+
+signal money_card_selected
+
+const MAX_MONEY_MARKET_CARDS = 4
+
+var card_scene = preload("res://Drawable/Card/MoneyCardDrawable.tscn")
+
+
+func cards_can_be_refilled():
+	return _get_active_money_card_count() < MAX_MONEY_MARKET_CARDS
+
+
+func _get_active_money_card_count() -> int:
+	return get_child_count()
+
+
+func refill():
+	var cards_to_refill_count = MAX_MONEY_MARKET_CARDS - _get_active_money_card_count()
+	for _i in range(cards_to_refill_count):
+		var card = Global.stack_money.take_card()
+		var card_node = card_scene.instance()
+		card_node._card_info = card
+		add_child(card_node)
+		card_node.connect("pressed", self, "_on_MoneyCard_pressed", [card_node])
+
+
+func _on_MoneyCard_pressed(card_node: MoneyCardDrawable):
+	card_node.disconnect("pressed", self, "_on_MoneyCard_pressed")
+	Global.active_player.money_cards.add_card(card_node._card_info)
+	remove_child(card_node)
+	emit_signal("money_card_selected", card_node)
