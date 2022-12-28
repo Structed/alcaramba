69f77be8f8219380ad021e45c9b0dd0ce160dc25
Johannes Ebner <Structed@users.noreply.github.com>
2022-12-29 00:57:13 +0100

    Fixed bug, where displayed walls were not properly reset when re-using a TileCardDrawable

--- src/alcaramba/Drawable/Card/TileCardDrawable.gd
+++ src/alcaramba/Drawable/Card/TileCardDrawable.gd
@@ -7,18 +7,19 @@ var _card_info: TileCard
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	update_control()
-	
+
 func update_control() -> void:
 	if _card_info:
 		$Value.text = str(_card_info._value)
 		$Value.self_modulate = Color(0,0,0)
 		self_modulate = _card_info.get_color()
+		reset_walls()
 		draw_walls()
 
 func set_card_info(card_info: TileCard):
 	_card_info = card_info
 	update_control()
-	
+
 func draw_walls():
 	var walls = _card_info.get_enabled_walls()
 	if walls["TOP"]:
@@ -29,3 +30,9 @@ func draw_walls():
 		$Panel_WallLeft.visible = true
 	if walls["RIGHT"]:
 		$Panel_WallRight.visible = true
+
+func reset_walls():
+	$Panel_WallTop.visible = false
+	$Panel_WallBottom.visible = false
+	$Panel_WallLeft.visible = false
+	$Panel_WallRight.visible = false
