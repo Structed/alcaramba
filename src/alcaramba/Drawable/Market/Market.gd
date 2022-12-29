6837635d4cb31656b860ac8f116b1cf06e3aeff9
Johannes Ebner <Structed@users.noreply.github.com>
2022-12-29 01:00:15 +0100

    Shuffling of tile stack is now in Global instead of Market

--- src/alcaramba/Drawable/Market/Market.gd
+++ src/alcaramba/Drawable/Market/Market.gd
@@ -11,7 +11,6 @@ onready var money_market = get_node("%MoneyMarket")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Global.stack_tiles.shuffle()
 	money_market.connect("money_card_selected", self, "_on_money_card_selected")
 	_on_EndTurnButton_pressed()
 
