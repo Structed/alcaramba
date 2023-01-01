122644e8c689377062a4e86ca3f5aea40f3d7ad7
zientist <jakob.ebner@gmail.com>
2023-01-01 11:51:24 +0100

    added containers to spare parts

--- src/alcaramba/Drawable/Town/SpareTiles.gd
+++ src/alcaramba/Drawable/Town/SpareTiles.gd
@@ -7,13 +7,14 @@ var tile_scene = preload("res://Drawable/Card/TileCardDrawable.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_stack_spare_tiles.initialize_for_game_start() # DEBUG
+	# _display_tiles()
 
 # STUB: paints all tiles from stack_spare_tiles
 func _display_tiles():
-	for _tile in _stack_spare_tiles:
+	for _i in range(_stack_spare_tiles.get_card_count()):
 		var tile_node = tile_scene.instance()
-		tile_node._card_info = _tile
+		tile_node._card_info = _stack_spare_tiles.get_card_info_by_index(_i)
 		add_child(tile_node)
 	
 	
