396e5cda39f4f932561e2ebf1537ebe204688a4d
zientist <jakob.ebner@gmail.com>
2023-01-06 15:16:01 +0100

    fixed multiple drawing of same tile

--- src/alcaramba/Drawable/Town/SpareTiles.gd
+++ src/alcaramba/Drawable/Town/SpareTiles.gd
@@ -1,22 +1,26 @@
 extends VBoxContainer
 
 var _stack_spare_tiles: TileCardCollection = TileCardCollection.new() # contains tiles placed on spare
+var _displayed_tiles: int = 0
 
 var tile_scene = preload("res://Drawable/Card/TileCardDrawable.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#_stack_spare_tiles.initialize_for_game_start() # DEBUG
-	#_display_tiles()
 	pass
 
-# STUB: paints all tiles from stack_spare_tiles
+# paints all tiles from stack_spare_tiles
 func _display_tiles():
+	# delete already displayed tiles
+	for n in self.get_children():
+		n.queue_free()
+	# create new TileCardDrawable for each til in stack_spare_tiles
 	for _i in range(_stack_spare_tiles.get_card_count()):
 		var tile_node = tile_scene.instance()
 		tile_node._card_info = _stack_spare_tiles.get_card_info_by_index(_i)
 		add_child(tile_node)
+		var children = self.get_children()
 
 func receive_tile(tile: TileCard):
 	pass
