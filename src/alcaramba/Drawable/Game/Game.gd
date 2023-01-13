25b2b6c36461df64c1fcc2726459fcc91274d401
Johannes Ebner <Structed@users.noreply.github.com>
2023-01-13 22:40:49 +0100

    Re-organised debug drawing to be in Global, instead of Game

--- src/alcaramba/Drawable/Game/Game.gd
+++ src/alcaramba/Drawable/Game/Game.gd
@@ -6,9 +6,6 @@ var _selected_tile: TileCardDrawable
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	OverlayDebugInfo.show()
-	OverlayDebugInfo.set_horizontal_align_right()
-	OverlayDebugInfo.set_vertical_align_top()
 
 
 	$Market.get_node("TileMarket").connect("tile_card_selected", self, "_on_tile_card_selected")
