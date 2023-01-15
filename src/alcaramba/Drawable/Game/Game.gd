ef8097352e8f15b0f4cb3d13a23cd36984341191
zientist <jakob.ebner@gmail.com>
2023-01-15 20:52:42 +0100

    implemented removal from spare stack if tile is placed

--- src/alcaramba/Drawable/Game/Game.gd
+++ src/alcaramba/Drawable/Game/Game.gd
@@ -9,26 +9,27 @@ onready var _market := get_node("%Market")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 
-
-
 	_market.get_node("TileMarket").connect("tile_card_selected", self, "_on_tile_card_selected")
 	_town.get_node("ContainerRight").get_node("SpareTiles").connect("tile_card_selected", self, "_on_tile_card_selected")
 
 
-
 func _on_tile_card_selected(card_node: TileCardDrawable):
-	# If another tile was clicked, we need to first reset the previous tile
-	if _selected_tile != null && card_node != _selected_tile:
-		_selected_tile.reset()
-
+	
+	_reset_selected_tile()
 	_selected_tile = card_node
+	
 	if _town.is_connected("tile_placed", self, "_on_tile_placed"):
 		_town.disconnect("tile_placed", self, "_on_tile_placed")
 	_town.connect("tile_placed", self, "_on_tile_placed")
 	_town.receive_tile(_selected_tile)
 	_selected_tile.select()
 
-
 func _on_tile_placed(tile: TileCard, position: Vector2):
 	Global.active_player.add_town_tile(tile, position)
 	_selected_tile.clear()
+
+func _reset_selected_tile():
+	# If a tile was clicked, we need to first reset the previous tile
+	if _selected_tile != null:
+			_selected_tile.reset()
+	_selected_tile = null
--- src/alcaramba/Drawable/Town/SpareTiles.gd
+++ src/alcaramba/Drawable/Town/SpareTiles.gd
@@ -22,11 +22,18 @@ func _display_tiles():
 		var tile_node = tile_scene.instance()
 		tile_node._card_info = _stack_spare_tiles.get_card_info_by_index(_i)
 		add_child(tile_node)
+		tile_node.connect("pressed", self, "_on_TileCard_pressed", [tile_node])
+
+func _on_TileCard_pressed(tile_node: TileCardDrawable):
+	tile_node.select()
+	emit_signal("tile_card_selected", tile_node)
 
 # removed tile from town is added to spare parts
 func _on_TileMap_tile_removed(tile: TileCard):
 	_stack_spare_tiles.add_card(tile)
 	_display_tiles()
 
-func _on_TileCard_pressed(card_node: TileCardDrawable):
-	emit_signal("tile_card_selected", card_node)
+# if placed tile came frome spare remove it from its stack, position is not needed here but emitted for other calls
+func _on_TileMap_tile_placed(tile: TileCard, _position: Vector2):
+	_stack_spare_tiles.remove_card_by_id(tile.get_id())
+	_display_tiles()
