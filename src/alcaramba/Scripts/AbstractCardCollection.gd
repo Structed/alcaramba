b66f21ab89ebbd6e39f8c5712caa6cea3308b84c
Johannes Ebner <Structed@users.noreply.github.com>
2023-02-06 22:50:56 +0100

    Add methods `has_card()` and `has_card_by_id()` to AbstractCardCollection.gd

--- src/alcaramba/Scripts/AbstractCardCollection.gd
+++ src/alcaramba/Scripts/AbstractCardCollection.gd
@@ -21,6 +21,23 @@ func get_card_info_by_id(id: int):
 			return card
 	return null
 
+# Checks whether a `card` exists in the collection
+# 
+# @param card: AbstractCard
+# @returns: bool - Whether the card exists in the Collection
+func has_card(card: AbstractCard) -> bool:
+	return has_card_by_id(card.get_id())
+
+# Checks whether a card with a given `id` exists in the collection
+# 
+# @param id: int
+# @returns: bool - Whether the card exists in the Collection
+func has_card_by_id(id: int) -> bool:
+	if get_card_info_by_id(id) == null:
+		return false
+		
+	return true
+
 func get_card_info_by_index(index: int):
 	return _stack[index]
 
