f8fea6fdd4cb6a33f076b709221da6b0e3f9b10d
Johannes Ebner <Structed@users.noreply.github.com>
2023-02-07 00:38:56 +0100

    Continuous update/display process

--- src/alcaramba/Drawable/Game/SpareTiles.gd
+++ src/alcaramba/Drawable/Game/SpareTiles.gd
@@ -5,18 +5,23 @@ signal tile_card_selected
 
 var tile_scene = preload("res://Drawable/Card/TileCardDrawable.tscn")
 
-# paints all tiles from stack_spare_tiles
-func _display_tiles():
-	# delete already displayed tiles
-	for n in self.get_children():
-		n.queue_free()
+var _cached_number_of_tiles = 0
 
-	# create new TileCardDrawable for each til in stack_spare_tiles
-	for _i in range(Global.active_player.tile_cards_yard.get_card_count()):
-		var tile_node = tile_scene.instance()
-		tile_node._card_info = Global.active_player.tile_cards_yard.get_card_info_by_index(_i)
-		add_child(tile_node)
-		tile_node.connect("pressed", self, "_on_TileCard_pressed", [tile_node])
+# paints all tiles from stack_spare_tiles
+func _process(_delta):
+	if _cached_number_of_tiles != Global.active_player.tile_cards_yard.get_card_count():
+		_cached_number_of_tiles = Global.active_player.tile_cards_yard.get_card_count()
+		
+		# delete already displayed tiles
+		for n in self.get_children():
+			n.queue_free()
+
+		# create new TileCardDrawable for each til in stack_spare_tiles
+		for _i in range(Global.active_player.tile_cards_yard.get_card_count()):
+			var tile_node = tile_scene.instance()
+			tile_node._card_info = Global.active_player.tile_cards_yard.get_card_info_by_index(_i)
+			add_child(tile_node)
+			tile_node.connect("pressed", self, "_on_TileCard_pressed", [tile_node])
 
 
 # Event handler for when a TileCard in the Spare Yard is pressed.
@@ -35,7 +40,6 @@ func _on_TileCard_pressed(tile_node: TileCardDrawable):
 # @returns: void
 func _on_add_to_spares(tile: TileCard):
 	Global.active_player.add_tile_to_spare_yard(tile)
-	_display_tiles()
 
 
 # If a tile is placed in town, remove it from its spare stack.
