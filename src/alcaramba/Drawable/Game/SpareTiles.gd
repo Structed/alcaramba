3597ee30b0664ee624312cdbd4a6e52f3c7c24e9
Johannes Ebner <Structed@users.noreply.github.com>
2023-02-06 22:19:03 +0100

    SpareTiles.gd refactoring
    - Cleanup comments, docs
    - Use `tile_cards_yard` from active player
    - Use Global.active_player.transfer_tile_spare_to_town(tile, position) to transfer tile to Town

--- src/alcaramba/Drawable/Game/SpareTiles.gd
+++ src/alcaramba/Drawable/Game/SpareTiles.gd
@@ -1,8 +1,10 @@
 extends VBoxContainer
 
+# Receives a `TileCardDrawable` as parameter
 signal tile_card_selected
 
-var _stack_spare_tiles: TileCardCollection = TileCardCollection.new() # contains tiles placed on spare
+# Receives a `TileCard` as parameter
+signal tile_card_added
 
 var tile_scene = preload("res://Drawable/Card/TileCardDrawable.tscn")
 
@@ -13,34 +15,41 @@ func _display_tiles():
 		n.queue_free()
 
 	# create new TileCardDrawable for each til in stack_spare_tiles
-	for _i in range(_stack_spare_tiles.get_card_count()):
+	for _i in range(Global.active_player.tile_cards_yard.get_card_count()):
 		var tile_node = tile_scene.instance()
-		tile_node._card_info = _stack_spare_tiles.get_card_info_by_index(_i)
+		tile_node._card_info = Global.active_player.tile_cards_yard.get_card_info_by_index(_i)
 		add_child(tile_node)
 		tile_node.connect("pressed", self, "_on_TileCard_pressed", [tile_node])
 
 
+# Event handler for when a TileCard in the Spare Yard is pressed.
+#
+# @visibility: private
+# @param tile_node: TileCardDrawable - The Tile node that was pressed
 func _on_TileCard_pressed(tile_node: TileCardDrawable):
 	tile_node.select()
 	emit_signal("tile_card_selected", tile_node)
 
 
 # removed tile from town is added to spare parts
-# visibility: public
+#
+# @visibility: public
 # @param tile: TileCard - The Tile Card to be placed in the yard
 # @returns: void
 func _on_add_to_spares(tile: TileCard):
 	if tile == null:
 		push_error("Tile passed to spare yard cannot be null!")
-	_stack_spare_tiles.add_card(tile)
+	Global.active_player.tile_cards_yard.add_card(tile)
 	_display_tiles()
+	emit_signal("tile_card_added", tile)
 
 
-# If a tile is placed in town, remove it from its spare stack. If it came from market, it is not in spare stack and nothing happens. 
+# If a tile is placed in town, remove it from its spare stack. If it came from market, it is not in spare stack and nothing happens.
 # _position is included in signal for other uses but not used here
+#
 # @visibility: public
 # @param tile: TileCard - The Tile Card to be placed in the yard
-# @param _position: Vector2 - Unused
+# @param position: Vector2 - The position of the Tile in Town
 # @returns: void
-func _on_TileMap_tile_placed(tile: TileCard, _position: Vector2):
-	_stack_spare_tiles.remove_card_by_id(tile.get_id())
+func _on_TileMap_tile_placed(tile: TileCard, position: Vector2):
+	Global.active_player.transfer_tile_spare_to_town(tile, position)
